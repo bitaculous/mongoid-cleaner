aac36bbe56e8caaf8f901247f28409bfbafd5e83
Maik Kempe <mkempe@bitaculous.com>
2015-10-16 16:14:33 +0200

    [➠] Extracted build steps for MongoDB on Travis CI into own script.

---
+++ resources/travis/scripts/mongodb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+MONGODB_VERSION='3.0.7'
+
+set -ev
+
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz -O /tmp/mongodb.tgz
+tar -xvf /tmp/mongodb.tgz
+mkdir /tmp/data
+${PWD}/mongodb-linux-x86_64-${MONGODB_VERSION}/bin/mongod --dbpath /tmp/data --bind_ip 127.0.0.1 --noauth &> /dev/null &
+
+# Try to keep environment pollution down, EPA loves us.
+unset MONGODB_VERSION
